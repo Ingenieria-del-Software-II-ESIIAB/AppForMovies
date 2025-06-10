afa7814c2e814c2aa5260f9b088d2cde2623d312
UCLM\Elena.Navarro <elena.navarro@uclm.es>
2025-06-10 15:22:50 +0200

    creating solution for AppForMovies running commands.bat

---
+++ test/AppForMovies.UIT/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace AppForMovies.UIT;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/AppForMovies.UT/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace AppForMovies.UT;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
