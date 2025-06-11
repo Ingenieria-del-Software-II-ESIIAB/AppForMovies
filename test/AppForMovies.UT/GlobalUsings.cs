e6a43488676132af785dc8518429cf692140bcf0
ELENA MARIA NAVARRO MARTINEZ <Elena.Navarro@uclm.es>
2025-06-11 14:37:24 +0200

    updating the UT project

--- test/AppForMovies.UT/GlobalUsings.cs
+++ test/AppForMovies.UT/GlobalUsings.cs
@@ -1,1 +1,17 @@
 global using Xunit;
+global using Microsoft.Data.Sqlite;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Mvc;
+global using System;
+global using System.Collections.Generic;
+global using System.Data.Common;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using AppForMovies.API.Data;
+global using AppForMovies.API.Models;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.Logging;
+global using Moq;
+
+
