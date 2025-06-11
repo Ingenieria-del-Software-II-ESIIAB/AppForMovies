0d0efedb2c5827f448a6978fe8abebf52ecca468
ELENA MARIA NAVARRO MARTINEZ <Elena.Navarro@uclm.es>
2025-06-11 14:08:00 +0200

    updating program and defining the connection string

---
+++ src/AppForMovies.API/Using.cs
@@ -0,0 +1,21 @@
+﻿global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.Extensions.DependencyInjection;
+global using System.Net;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using AppForMovies.API.Models;
+global using AppForMovies.API.Data;
+global using System.Text.Json.Serialization;
+global using Microsoft.OpenApi.Models;
+global using Swashbuckle.AspNetCore.SwaggerGen;
+global using System.Reflection;
+
+
