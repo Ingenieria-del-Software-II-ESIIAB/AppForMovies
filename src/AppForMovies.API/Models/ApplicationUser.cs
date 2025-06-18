c3eeef2f17a29524c5c9d9382f9edcbf86d9a6a0
Juanlr13 <136679799+Juanlr13@users.noreply.github.com>
2025-06-18 12:33:46 +0200

    PR: Create the Model Classes for Renting Movies. Closes #12 (#16)
    * Create Genre class
    
    * Create Movie class
    
    * Update the Movie class to enforce uniqueness of the attribute "Title".
    
    * Create Rental class
    
    * Update ApplicationUser
    
    * Specify the class diagram for Rental Movies
    
    * Adding Use Case - Rental Movies documentation
    
    * Resolving mistakes related to Movie Class

--- src/AppForMovies.API/Models/ApplicationUser.cs
+++ src/AppForMovies.API/Models/ApplicationUser.cs
@@ -5,5 +5,30 @@ namespace AppForMovies.API.Models;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    public ApplicationUser()
+    {
+    }
+    public ApplicationUser(string id, string name, string surname, string userName)
+    {
+        Id = id;
+        Name = name;
+        Surname = surname;
+        UserName = userName;
+        Email = userName;
+    }
+
+    [Display(Name = "Name")]
+    public string? Name
+    {
+        get;
+        set;
+    }
+
+    [Display(Name = "Surname")]
+    public string? Surname
+    {
+        get;
+        set;
+    }
 }
 
---
+++ src/AppForMovies.API/Models/RentalItem.cs
@@ -0,0 +1,45 @@
+﻿namespace AppForMovies.API.Models
+{
+    public class RentalItem
+    {
+        public RentalItem()
+        {
+        }
+
+        public RentalItem(Movie movie, Rental rent)
+        {
+            Movie = movie;
+            MovieId = movie.Id;
+            Rent = rent;
+            RentalId = rent.Id;
+        }
+
+        public RentalItem(Movie movie, Rental rent, string? description) : this(movie, rent)
+        {
+            Description = description;
+            PriceForRenting = movie.PriceForRenting;
+        }
+
+        public RentalItem(int movieId, Rental rental, double priceForRenting)
+        {
+            MovieId = movieId;
+            Rent = rental;
+            PriceForRenting = priceForRenting;
+        }
+
+        public RentalItem(int movieId, Rental rental, double priceForRenting, string? description) : this(movieId, rental, priceForRenting) => Description = description;
+
+        public Movie Movie { get; set; }
+
+        public int MovieId { get; set; }
+
+
+        public Rental Rent { get; set; }
+
+        public int RentalId { get; set; }
+
+        [StringLength(100, ErrorMessage = "Title name cannot be longer than 50 characters.")]
+        public string? Description { get; set; }
+        public double PriceForRenting { get; set; }
+    }   
+}
