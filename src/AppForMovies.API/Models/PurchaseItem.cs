6a8159b7fc952ed5ca9b0617de67a78aaf04329a
Juanlr13 <136679799+Juanlr13@users.noreply.github.com>
2025-06-20 12:47:16 +0200

    PR: Create the Model Classes for Purchase Movies. Closes #18 (#19)
    Create Purchase class and update Movie attributes related with Purchase

---
+++ src/AppForMovies.API/Models/PurchaseItem.cs
@@ -0,0 +1,35 @@
+﻿namespace AppForMovies.API.Models
+{
+    [PrimaryKey(nameof(MovieId), nameof(PurchaseId))]
+    public class PurchaseItem
+    {
+        public PurchaseItem()
+        {
+        }
+
+        public PurchaseItem(Movie movie, Purchase purchase, int quantity)
+        {
+            Movie = movie;
+            MovieId = movie.Id;
+            Purchase = purchase;
+            PurchaseId = purchase.Id;
+            Price = movie.PriceForPurchase;
+            Quantity = quantity;
+        }
+
+        public Movie Movie { get; set; }
+
+        public int MovieId { get; set; }
+
+        public Purchase Purchase { get; set; }
+
+        public int PurchaseId { get; set; }
+
+        [Precision(10, 2)]
+        public decimal Price { get; set; }
+
+
+        [Range(1, int.MaxValue, ErrorMessage = "You must provide a quantity higher than 1")]
+        public int Quantity { get; set; }
+    }
+}
