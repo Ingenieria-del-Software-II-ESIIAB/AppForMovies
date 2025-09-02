6fa9533e2a51adf99736b1f9b034cd9725ba079b
Juanlr13 <juan.lopez17@alu.uclm.es>
2025-09-02 10:38:40 +0200

    Create the RentalsController with the _context variable to access the database and _logger to log any information during execution.

---
+++ src/AppForMovies.API/Controllers/RentalsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForMovies.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<RentalsController> _logger;
+
+        public RentalsController(ApplicationDbContext context, ILogger<RentalsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+    }
+
+}
