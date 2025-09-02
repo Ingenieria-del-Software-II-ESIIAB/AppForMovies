aee9d3ab2071a1e177c39e15dda6a6a6945769e0
Juanlr13 <juan.lopez17@alu.uclm.es>
2025-09-02 12:05:36 +0200

    Adding GetRental in RentalsController

--- src/AppForMovies.API/Controllers/RentalsController.cs
+++ src/AppForMovies.API/Controllers/RentalsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AppForMovies.API.DTOs.RentalDTOs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppForMovies.API.Controllers
@@ -14,6 +15,44 @@ namespace AppForMovies.API.Controllers
         {
             _context = context;
             _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(RentalDetailDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetRental(int id)
+        {
+            if (_context.Rentals == null)
+            {
+                _logger.LogError("Error: Rentals table does not exist");
+                return NotFound();
+            }
+
+            var rental = await _context.Rentals
+             .Where(r => r.Id == id)
+                 .Include(r => r.RentalItems) //join table RentalItems
+                    .ThenInclude(ri => ri.Movie) //then join table Movies
+                        .ThenInclude(movie => movie.Genre) //then join table Genre
+             .Select(r => new RentalDetailDTO(r.Id, r.RentalDate, r.CustomerUserName,
+                    r.CustomerNameSurname, r.DeliveryAddress,
+                    (PaymentMethodTypes)r.PaymentMethod,
+                    r.RentalDateFrom, r.RentalDateTo,
+                    r.RentalItems
+                        .Select(ri => new RentalItemDTO(ri.Movie.Id,
+                                ri.Movie.Title, ri.Movie.Genre.Name,
+                                ri.Movie.PriceForRenting, ri.Description)).ToList<RentalItemDTO>()))
+             .FirstOrDefaultAsync();
+
+
+            if (rental == null)
+            {
+                _logger.LogError($"Error: Rental with id {id} does not exist");
+                return NotFound();
+            }
+
+
+            return Ok(rental);
         }
     }
 
