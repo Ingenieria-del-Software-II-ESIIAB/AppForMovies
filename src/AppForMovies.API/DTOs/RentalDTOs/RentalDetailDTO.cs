9bbbe1a0ad3fadf3316daae5639e32c71aec71e1
Juanlr13 <juan.lopez17@alu.uclm.es>
2025-09-02 11:48:07 +0200

    Create DTOs for Rental

---
+++ src/AppForMovies.API/DTOs/RentalDTOs/RentalDetailDTO.cs
@@ -0,0 +1,57 @@
+﻿
+namespace AppForMovies.API.DTOs.RentalDTOs
+{
+    public class RentalDetailDTO : RentalForCreateDTO
+    {
+        public RentalDetailDTO(int id, DateTime rentalDate, string customerUserName, string customerNameSurname,
+            string deliveryAddress, PaymentMethodTypes paymentMethod, DateTime rentalDateFrom,
+            DateTime rentalDateTo, IList<RentalItemDTO> rentalItems)
+            : base(customerUserName,
+                   customerNameSurname,
+                   deliveryAddress,
+                   paymentMethod,
+                   rentalDateFrom,
+                   rentalDateTo,
+                   rentalItems)
+        {
+            Id = id;
+            RentalDate = rentalDate;
+        }
+        public int Id { get; set; }
+
+        public DateTime RentalDate { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is RentalDetailDTO dTO &&
+                   base.Equals(obj) &&
+                   RentalDateFrom == dTO.RentalDateFrom &&
+                   RentalDateTo == dTO.RentalDateTo &&
+                   DeliveryAddress == dTO.DeliveryAddress &&
+                   CustomerUserName == dTO.CustomerUserName &&
+                   CustomerNameSurname == dTO.CustomerNameSurname &&
+                   EqualityComparer<IList<RentalItemDTO>>.Default.Equals(RentalItems, dTO.RentalItems) &&
+                   PaymentMethod == dTO.PaymentMethod &&
+                   TotalPrice == dTO.TotalPrice &&
+                   Id == dTO.Id &&
+                   RentalDate == dTO.RentalDate;
+        }
+
+        public override int GetHashCode()
+        {
+            HashCode hash = new HashCode();
+            hash.Add(base.GetHashCode());
+            hash.Add(RentalDateFrom);
+            hash.Add(RentalDateTo);
+            hash.Add(DeliveryAddress);
+            hash.Add(CustomerUserName);
+            hash.Add(CustomerNameSurname);
+            hash.Add(RentalItems);
+            hash.Add(PaymentMethod);
+            hash.Add(TotalPrice);
+            hash.Add(Id);
+            hash.Add(RentalDate);
+            return hash.ToHashCode();
+        }
+    }
+}
---
+++ src/AppForMovies.API/DTOs/RentalDTOs/RentalItemDTO.cs
@@ -0,0 +1,42 @@
+﻿
+namespace AppForMovies.API.DTOs.RentalDTOs
+{
+    public class RentalItemDTO
+    {
+        public RentalItemDTO(int movieID, string title, string genre, double priceForRenting, string description = "")
+        {
+            MovieID = movieID;
+            Title = title;
+            PriceForRenting = priceForRenting;
+            Genre = genre;
+            Description = description;
+        }
+
+        public int MovieID { get; set; }
+
+
+        public string Title { get; set; }
+
+
+        public double PriceForRenting { get; set; }
+
+        public string? Description { get; set; }
+
+        public string Genre { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is RentalItemDTO dTO &&
+                   MovieID == dTO.MovieID &&
+                   Title == dTO.Title &&
+                   PriceForRenting == dTO.PriceForRenting &&
+                   Description == dTO.Description &&
+                   Genre == dTO.Genre;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(MovieID, Title, PriceForRenting, Description, Genre);
+        }
+    }
+}
