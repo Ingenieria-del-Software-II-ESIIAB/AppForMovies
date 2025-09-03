ae6bdc03ea7efcf3807bb3fe881807cfe36d5888
Elena Navarro <elena.navarro@uclm.es>
2025-09-03 14:03:32 +0200

    PR for Task22 develop post method for creating a rental (#27)
    * Developing Post CreateRental
    
    * adding the UT for Post CreateRental
    
    * adding the constructor to PostRental_test for feeding the database for UT
    
    * adding the UTs for testing Post CreateRental

--- src/AppForMovies.API/Controllers/RentalsController.cs
+++ src/AppForMovies.API/Controllers/RentalsController.cs
@@ -54,6 +54,103 @@ namespace AppForMovies.API.Controllers
 
             return Ok(rental);
         }
+
+        [HttpPost]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(RentalDetailDTO), (int)HttpStatusCode.Created)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.Conflict)]
+        public async Task<ActionResult> CreateRental(RentalForCreateDTO rentalForCreate) {
+            //any validation defined in PurchaseForCreate is checked before running the method so they don't have to be checked again
+            if (rentalForCreate.RentalDateFrom <= DateTime.Today)
+                ModelState.AddModelError("RentalDateFrom", "Error! Your rental date must start later than today");
+
+            if (rentalForCreate.RentalDateFrom >= rentalForCreate.RentalDateTo)
+                ModelState.AddModelError("RentalDateFrom&RentalDateTo", "Error! Your rental must end later than it starts");
+
+            if (rentalForCreate.RentalItems.Count == 0)
+                ModelState.AddModelError("RentalItems", "Error! You must include at least one movie to be rented");
+
+            // if (!_context.ApplicationUsers.Any(au=>au.UserName==rentalForCreate.CustomerUserName))
+            var user = _context.ApplicationUsers.FirstOrDefault(au => au.UserName == rentalForCreate.CustomerUserName);
+            if (user == null)
+                ModelState.AddModelError("RentalApplicationUser", "Error! UserName is not registered");
+
+            if (ModelState.ErrorCount > 0)
+                return BadRequest(new ValidationProblemDetails(ModelState));
+
+
+            var movieTitles = rentalForCreate.RentalItems.Select(ri => ri.Title).ToList<string>();
+
+            var movies = _context.Movies.Include(m => m.RentalItems)
+                .ThenInclude(ri => ri.Rent)
+                .Where(m => movieTitles.Contains(m.Title))
+
+                //we use an anonymous type https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/types/anonymous-types
+                .Select(m => new {
+                    m.Id,
+                    m.Title,
+                    m.QuantityForRenting,
+                    m.PriceForRenting,
+                    //we count the number of rentalItems that are within the rental period
+                    NumberOfRentedItems = m.RentalItems.Count(ri => ri.Rent.RentalDateFrom <= rentalForCreate.RentalDateTo
+                            && ri.Rent.RentalDateTo >= rentalForCreate.RentalDateFrom)
+                })
+                .ToList();
+
+
+            Rental rental = new Rental(rentalForCreate.CustomerUserName, rentalForCreate.CustomerNameSurname,
+                user, rentalForCreate.DeliveryAddress, DateTime.Now,
+                (AppForMovies.API.Models.PaymentMethodTypes)rentalForCreate.PaymentMethod,
+rentalForCreate.RentalDateFrom, rentalForCreate.RentalDateTo, new List<RentalItem>());
+
+
+            rental.TotalPrice = 0;
+            var numDays = (rental.RentalDateTo - rental.RentalDateFrom).TotalDays;
+
+
+            foreach (var item in rentalForCreate.RentalItems) {
+                var movie = movies.FirstOrDefault(m => m.Title == item.Title);
+                //we must check that there is enough quantity to be rented in the database
+                if ((movie == null) || (movie.NumberOfRentedItems >= movie.QuantityForRenting)) {
+                    ModelState.AddModelError("RentalItems", $"Error! Movie titled '{item.Title}' is not available for being rented from {rentalForCreate.RentalDateFrom.ToShortDateString()} to {rentalForCreate.RentalDateTo.ToShortDateString()}");
+                }
+                else {
+                    // rental does not exist in the database yet and does not have a valid Id, so we must relate rentalitem to the object rental
+                    rental.RentalItems.Add(new RentalItem(movie.Id, rental, movie.PriceForRenting, item.Description));
+                    item.PriceForRenting = movie.PriceForRenting;
+                }
+            }
+            rental.TotalPrice = rental.RentalItems.Sum(ri => ri.PriceForRenting * numDays);
+
+
+            //if there is any problem because of the available quantity of movies or because the movie does not exist
+            if (ModelState.ErrorCount > 0) {
+                return BadRequest(new ValidationProblemDetails(ModelState));
+            }
+
+            _context.Add(rental);
+
+            try {
+                //we store in the database both rental and its rentalitems
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex) {
+                _logger.LogError(ex.Message);
+                ModelState.AddModelError("Rental", $"Error! There was an error while saving your rental, plese, try again later");
+                return Conflict("Error" + ex.Message);
+
+            }
+
+            //it returns rentalDetail
+            var rentalDetail = new RentalDetailDTO(rental.Id, rental.RentalDate,
+                rental.CustomerUserName, rental.CustomerNameSurname,
+                rental.DeliveryAddress, rentalForCreate.PaymentMethod,
+                rental.RentalDateFrom, rental.RentalDateTo,
+                rentalForCreate.RentalItems);
+
+            return CreatedAtAction("GetRental", new { id = rental.Id }, rentalDetail);
+        }
     }
 
 }
---
+++ test/AppForMovies.UT/RentalsController_test/PostRentals_test.cs
@@ -0,0 +1,104 @@
+﻿using AppForMovies.API.Controllers;
+using AppForMovies.API.DTOs.RentalDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForMovies.UT.RentalsController_test {
+    public class PostRentals_test:AppForMovies4SqliteUT {
+        public PostRentals_test() {
+
+            var genres = new List<Genre>() {
+                new Genre("Sci - Fi"),
+                new Genre("Drama"),
+            };
+
+            var movies = new List<Movie>(){
+                new Movie("The lord of the rings", genres[0],new DateTime(2011, 10, 20),10.0m, 5,1.0,1),
+                new Movie("The man in the high castle", genres[1],new DateTime(2015, 01, 01),10.0m,0, 4.0,15),
+            };
+
+            ApplicationUser user = new ApplicationUser("1", "Elena", "Navarro Martínez", "elena@uclm.es");
+
+            var rental = new Rental("elena.navarro@uclm.es", "Elena Navarro",
+                   user, "Avda. España s/n, Albacete 02071",
+                    DateTime.Now, AppForMovies.API.Models.PaymentMethodTypes.CreditCard,
+                    DateTime.Today.AddDays(2), DateTime.Today.AddDays(5),
+                    new List<RentalItem>());
+            rental.RentalItems.Add(new RentalItem(movies[0], rental, "My favourite movie"));
+
+            _context.ApplicationUsers.Add(user);
+            _context.AddRange(genres);
+            _context.AddRange(movies);
+            _context.Add(rental);
+            _context.SaveChanges();
+        }
+
+        public static IEnumerable<object[]> TestCasesFor_CreatePurchase() {
+            var rentalNoITem = new RentalForCreateDTO("elena@uclm.es", "Elena Navarro",
+                "Avda. España s/n, Albacete", PaymentMethodTypes.CreditCard,
+                DateTime.Today.AddDays(2), DateTime.Today.AddDays(5), new List<RentalItemDTO>());
+
+            var rentalItems = new List<RentalItemDTO>() { new RentalItemDTO(2, "The man in the high castle", "Drama", 4.0, "My favourite movie") };
+
+            var rentalFromBeforeToday = new RentalForCreateDTO("elena@uclm.es", "Elena Navarro",
+                "Avda. España s/n, Albacete", PaymentMethodTypes.CreditCard,
+                DateTime.Today, DateTime.Today.AddDays(5), rentalItems);
+
+            var rentalToBeforeFrom = new RentalForCreateDTO("elena@uclm.es", "Elena Navarro",
+                "Avda. España s/n, Albacete", PaymentMethodTypes.CreditCard,
+                DateTime.Today.AddDays(5), DateTime.Today.AddDays(2), rentalItems);
+
+            var RentalApplicationUser = new RentalForCreateDTO("victor.lopez@uclm.es", "Elena Navarro",
+                "Avda. España s/n, Albacete", PaymentMethodTypes.CreditCard,
+                DateTime.Today.AddDays(2), DateTime.Today.AddDays(4), rentalItems);
+
+            var rentalMovieNotAvailable = new RentalForCreateDTO("elena@uclm.es", "Elena Navarro",
+                "Avda. España s/n, Albacete", PaymentMethodTypes.CreditCard,
+                DateTime.Today.AddDays(2), DateTime.Today.AddDays(5),
+                new List<RentalItemDTO>() { new RentalItemDTO(1, "The lord of the rings", "Sci - Fi", 1.0) });
+
+
+            var allTests = new List<object[]>
+            {             //input for createpurchase - Error expected
+                new object[] { rentalNoITem, "Error! You must include at least one movie to be rented",  },
+                new object[] { rentalFromBeforeToday, "Error! Your rental date must start later than today", },
+                new object[] { rentalToBeforeFrom, "Error! Your rental must end later than it starts", },
+                new object[] { RentalApplicationUser, "Error! UserName is not registered", },
+                new object[] { rentalMovieNotAvailable, "Error! Movie titled 'The lord of the rings' is not available for being rented from", },
+            };
+
+            return allTests;
+        }
+
+        [Theory]
+        [Trait("LevelTesting", "Unit Testing")]
+        [Trait("Database", "WithoutFixture")]
+        [MemberData(nameof(TestCasesFor_CreatePurchase))]
+        public async Task CreateRental_Error_test(RentalForCreateDTO rentalDTO, string errorExpected) {
+            // Arrange
+            var mock = new Mock<ILogger<RentalsController>>();
+            ILogger<RentalsController> logger = mock.Object;
+
+            var controller = new RentalsController(_context, logger);
+
+            // Act
+            var result = await controller.CreateRental(rentalDTO);
+
+            //Assert
+            //we check that the response type is BadRequest and obtain the error returned
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            var problemDetails = Assert.IsType<ValidationProblemDetails>(badRequestResult.Value);
+
+            var errorActual = problemDetails.Errors.First().Value[0];
+
+            //we check that the expected error message and actual are the same
+            Assert.StartsWith(errorExpected, errorActual);
+
+        }
+
+    }
+}
+
