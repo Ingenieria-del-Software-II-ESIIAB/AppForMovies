7812dd17c10dea0ce60df3cd90c24b3c5b1eeed9
ELENA MARIA NAVARRO MARTINEZ <Elena.Navarro@uclm.es>
2025-09-03 13:45:15 +0200

    adding the UT for Post CreateRental

---
+++ test/AppForMovies.UT/RentalsController_test/PostRentals_test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForMovies.UT.RentalsController_test {
+    public class PostRentals_test:AppForMovies4SqliteUT {
+
+    }
+}
