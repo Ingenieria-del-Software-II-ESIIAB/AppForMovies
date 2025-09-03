fad7ad3003e3826d375bcb46f8db48a0f3c416b7
ELENA MARIA NAVARRO MARTINEZ <Elena.Navarro@uclm.es>
2025-09-03 12:44:59 +0200

    seeding Movies and Genres

--- src/AppForMovies.API/Data/SeedData.cs
+++ src/AppForMovies.API/Data/SeedData.cs
@@ -18,6 +18,14 @@
             catch (Exception ex) {
                 logger.LogError(ex, "An error occurred seeding the Users in the Database.");
             }
+
+            try {
+                //it initializes the database with genres and movies
+                SeedGenresAndMovies(dbContext);
+            }
+            catch (Exception ex) {
+                logger.LogError(ex, "An error occurred seeding the Movies and Genres in the Database.");
+            }
         }
 
         public static void SeedRoles(RoleManager<IdentityRole> roleManager, List<string> roles) {
@@ -77,6 +85,50 @@
 
                 }
             }
+
+        }
+
+        public static void SeedGenresAndMovies(ApplicationDbContext dbcontext) {
+            string[] genresnames = ["Sci - Fi", "Drama", "Comedy", "Soap opera"];
+            List<Genre> genres = [];
+            Movie movie;
+            foreach (string genrename in genresnames) {
+                var genre = dbcontext.Genres.FirstOrDefault(g => g.Name == genrename);
+                if (genre == null)
+                    genres.Add(new Genre(genrename));
+                else
+                    genres.Add(genre);
+            }
+            if (dbcontext.Movies.FirstOrDefault(m => m.Title == "The last of us") == null) {
+                movie = new Movie("The last of us", genres[0], new DateTime(2023, 03, 15), 10.0m, 5, 1.0, 1);
+                dbcontext.Movies.Add(movie);
+
+            }
+
+            if (dbcontext.Movies.FirstOrDefault(m => m.Title == "The man in the high castle") == null) {
+                movie = new Movie("The man in the high castle", genres[1], new DateTime(2015, 01, 15), 20.0m, 15, 3.0, 10);
+                dbcontext.Movies.Add(movie);
+            }
+
+            //it saves the modification of dbcontext to the database
+            dbcontext.SaveChanges();
+
+            //alternatively you may have used a raw SQL
+            //dbcontext.Database.ExecuteSqlRaw("INSERT INTO [Movies] ([Id], [Title], [GenreId], [ReleaseDate], [PriceForPurchase], [QuantityForPurchase], [PriceForRenting], [QuantityForRenting]) VALUES (1, N'The lord of the rings', 1, N'2011-10-20 00:00:00', 10, 1000, 1, 100)");
+            //dbcontext.Database.ExecuteSqlRaw("INSERT INTO [Movies] ([Id], [Title], [GenreId], [ReleaseDate], [PriceForPurchase], [QuantityForPurchase], [PriceForRenting], [QuantityForRenting]) VALUES (2, N'The flying castle', 2, N'2007-04-04 00:00:00', 20, 1000, 3, 10)");
+
+
+            //Since EFCORE7, you can perform bulk updates with linq.
+            dbcontext.Movies.ExecuteUpdate(s => s.SetProperty(m => m.QuantityForPurchase, 10));
+
+            //other example using existing information: add 100 to the QuantityForPurchase of each Movie
+            //dbcontext.Movies.ExecuteUpdate(s => s.SetProperty(m => m.QuantityForPurchase, m=>m.QuantityForPurchase+100));
+
+            //You can alternatively use raw SQL to perform the operation where performance is sensitive:
+            //dbcontext.Database.ExecuteSqlRaw("UPDATE [Movies] SET [QuantityForPurchase] = 100");
+
+            dbcontext.SaveChanges();
+
 
         }
 
