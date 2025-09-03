9ffa7e71e714d9a6a6621a6ed8a52b83f8537b3e
ELENA MARIA NAVARRO MARTINEZ <Elena.Navarro@uclm.es>
2025-09-03 13:46:38 +0200

    adding the constructor to PostRental_test for feeding the database for UT

--- test/AppForMovies.UT/RentalsController_test/PostRentals_test.cs
+++ test/AppForMovies.UT/RentalsController_test/PostRentals_test.cs
@@ -6,6 +6,32 @@ using System.Threading.Tasks;
 
 namespace AppForMovies.UT.RentalsController_test {
     public class PostRentals_test:AppForMovies4SqliteUT {
+        public PostRentals_test() {
 
+            var genres = new List<Genre>() {
+                new Genre("Sci - Fi"),
+                new Genre("Drama"),
+            };
+
+            var movies = new List<Movie>(){
+                new Movie("The lord of the rings", genres[0],new DateTime(2011, 10, 20),10.0m, 5,1.0,1),
+                new Movie("The man in the high castle", genres[1],new DateTime(2015, 01, 01),10.0m,0, 4.0,15),
+            };
+
+            ApplicationUser user = new ApplicationUser("1", "Elena", "Navarro Martínez", "elena@uclm.es");
+
+            var rental = new Rental("elena.navarro@uclm.es", "Elena Navarro",
+                   user, "Avda. España s/n, Albacete 02071",
+                    DateTime.Now, AppForMovies.API.Models.PaymentMethodTypes.CreditCard,
+                    DateTime.Today.AddDays(2), DateTime.Today.AddDays(5),
+                    new List<RentalItem>());
+            rental.RentalItems.Add(new RentalItem(movies[0], rental, "My favourite movie"));
+
+            _context.ApplicationUsers.Add(user);
+            _context.AddRange(genres);
+            _context.AddRange(movies);
+            _context.Add(rental);
+            _context.SaveChanges();
+        }
     }
 }
