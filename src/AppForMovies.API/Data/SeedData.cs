052498c1d87cd0847495050512bf14737c47b759
ELENA MARIA NAVARRO MARTINEZ <Elena.Navarro@uclm.es>
2025-09-03 12:42:50 +0200

    seeding roles in the database

--- src/AppForMovies.API/Data/SeedData.cs
+++ src/AppForMovies.API/Data/SeedData.cs
@@ -1,8 +1,31 @@
 ﻿namespace AppForMovies.API.Data {
     public class SeedData {
         public static void Initialize(ApplicationDbContext dbContext, IServiceProvider serviceProvider, ILogger logger) {
+            List<string> rolesNames = new List<string> { "Administrator", "Employee", "Customer" };
+
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            try {
+                SeedRoles(roleManager, rolesNames);
+            }
+            catch (Exception ex) {
+                logger.LogError(ex, "An error occurred seeding the roles in the Database.");
+            }
+        }
+
+        public static void SeedRoles(RoleManager<IdentityRole> roleManager, List<string> roles) {
+
+            foreach (string roleName in roles) {
+                //it checks such role does not exist in the database 
+                if (!roleManager.RoleExistsAsync(roleName).Result) {
+                    IdentityRole role = new IdentityRole();
+                    role.Name = roleName;
+                    role.NormalizedName = roleName;
+                    IdentityResult roleResult = roleManager.CreateAsync(role).Result;
+                }
+            }
 
         }
+
 
     }
 }
