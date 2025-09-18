f96e3119982c80c46f03375cfc2d1816872d287e
Elena Navarro <elena.navarro@uclm.es>
2025-09-18 19:26:58 +0200

    Task36 adding a new service to the API for Genre (#37)
    * developing GenresController
    
    * updating the client

---
+++ src/AppForMovies.API/Controllers/GenresController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForMovies.API.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase {
+        private readonly ApplicationDbContext _context;
+        private ILogger _logger;
+
+        public GenresController(ApplicationDbContext context, ILogger<MoviesController> logger) {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: api/Movies/GetMoviesForPurchase
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<string>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetGenres(string? genreName) {
+
+            IList<string> genres = await _context.Genres
+                .Where(genre => (genreName == null || genre.Name.Contains(genreName))) // where clause             
+                .OrderBy(genre => genre.Name)
+                .Select(genre => genre.Name)
+                .ToListAsync();
+
+            return Ok(genres);
+        }
+    }
+}
