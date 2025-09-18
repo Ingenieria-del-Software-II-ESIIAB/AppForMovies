2cbb97a9095e70833429d97dda05307279b8b518
Elena Navarro <elena.navarro@uclm.es>
2025-09-18 19:07:48 +0200

    Task33 creating a state container for rental (#34)
    * creating rentalstatecontainer
    
    * adding the state container to program

---
+++ src/AppForMovies.Web/RentalStateContainer.cs
@@ -0,0 +1,49 @@
+﻿using AppForMovies.Web.API;
+
+namespace AppForMovies.Web {
+    public class RentalStateContainer {
+        public RentalForCreateDTO Rental { get; private set; } = new RentalForCreateDTO() {
+            RentalItems = new List<RentalItemDTO>()
+        };
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+
+        public void AddMovieToRental(MovieForRentalDTO movie) {
+            if (!Rental.RentalItems.Any(ri => ri.MovieID == movie.Id))
+                Rental.RentalItems.Add(new RentalItemDTO() {
+                    MovieID = movie.Id,
+                    Genre = movie.Genre,
+                    Title = movie.Title,
+                    PriceForRenting = movie.PriceForRenting,
+                }
+            );
+            ComputeTotalPrice();
+        }
+
+        private void ComputeTotalPrice() {
+            int numberOfDays = (Rental.RentalDateTo - Rental.RentalDateFrom).Days;
+            Rental.TotalPrice = Rental.RentalItems.Sum(ri => ri.PriceForRenting * numberOfDays);
+        }
+
+        public void RemoveRentalItemToRent(RentalItemDTO item) {
+            Rental.RentalItems.Remove(item);
+            ComputeTotalPrice();
+        }
+
+        public void ClearRentingCart() {
+            Rental.RentalItems.Clear();
+            Rental.TotalPrice = 0;
+        }
+
+        public void RentalProcessed() {
+            //we have finished the rental process so we create a new object without data
+            Rental = new RentalForCreateDTO() {
+                RentalItems = new List<RentalItemDTO>()
+            };
+        }
+
+    }
+}
