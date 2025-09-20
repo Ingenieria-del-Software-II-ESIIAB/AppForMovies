c78892e06539273bb72de97ea4f9a6fbaa41341f
UCLM\Elena.Navarro <elena.navarro@uclm.es>
2025-09-20 12:34:27 +0200

    adding RentalMoviesUIT

---
+++ test/AppForMovies.UIT/RentalMovies/UCRentalMovies_UIT.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForMovies.UIT.RentalMovies {
+    public class UCRentalMovies_UIT: UC_UIT {
+
+        public UCRentalMovies_UIT(ITestOutputHelper output) :base(output) { }
+
+    }
+}
